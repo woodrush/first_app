3f8ddcb37367fd657e88109ac6a44c39b148dd47
hikuta924 <hikuta924@gmail.com>
2013-08-12 21:20:24 -0700

    change controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
-  def new
-  end
+	def new
+		format.xml  { render :xml => "<Response><Sms>Thank you for using Trumo! Here is your number, so call them maybe... ------------</Sms></Response>", :status => :unprocessable_entity }
+	#   @users = User.find(:all)
+	#   file = File.new('dir.xml','w')
+	#   doc = Document.new
+
+	#   make = Element.new "make"
+	# 	@users.each do |y|
+	# 	  name_node = make.add_element "name"
+	# 	  name_node.text  = y.name
+	# 	  desc_node = make.add_element "description"
+	# 	  desc_node.text = y.description
+	# 	end
+
+	#   doc.add_element make
+
+	#   file.puts doc
+	#   file.close
+	# end
 end
